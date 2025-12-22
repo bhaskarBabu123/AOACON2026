6f184fc73a4b9df255390e5d1f000f8f8df3f7d6
bhaskarAntony <rooparoopa7105@gmail.com>
2025-12-22 10:42:25 +0530

    all files

--- src/pages/user/RegistrationFeesPage.jsx
+++ src/pages/user/RegistrationFeesPage.jsx
@@ -328,6 +328,7 @@ const RegistrationFeesPage = () => {
           </div>
         </section>
         <center>
+          {/* // */}
           <Link
           to="/registration"
              className="w-full px-6 py-3.5 rounded-xl border border-slate-200 bg-[#005aa9] text-white text-sm font-medium hover:from-[#00695c] hover:to-[#005aa9] disabled:opacity-50 disabled:cursor-not-allowed transition-colors flex items-center justify-center gap-2"
